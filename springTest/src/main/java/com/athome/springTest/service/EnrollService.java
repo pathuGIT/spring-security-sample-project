b5e3cbcc0c4d41fecab9cafa99a7e3f142a84b95
pathuGIT <wannigelakshan@gmail.com>
2025-08-09 13:32:21 +0530

    Update EnrollService.java
    use builder

--- springTest/src/main/java/com/athome/springTest/service/EnrollService.java
+++ springTest/src/main/java/com/athome/springTest/service/EnrollService.java
@@ -8,6 +8,8 @@ import com.athome.springTest.repository.AcademicRepository;
 import com.athome.springTest.repository.EnrollRepository;
 import com.athome.springTest.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Service;
 
@@ -25,16 +27,22 @@ public class EnrollService {
     @Autowired
     private UserRepository userRepository;
 
-    public Enrollments enroll(int academicCourse_id, int user_id) {
+    public Enrollments enroll(int academicCourse_id) {
+        // Get authentication object from SecurityContext
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        // Get username (usually the principal's name)
+        String username = authentication.getName();
+
         AcademicCourse academicCourse = academicCourseRepository.findById(academicCourse_id)
                 .orElseThrow(()-> new IllegalArgumentException("Not pound this academicCourse id: " + academicCourse_id));
-        Users user = userRepository.findById(user_id)
-                .orElseThrow(() -> new IllegalArgumentException("Not pound this user id: " + user_id));
+        Users user = userRepository.findByUsername(username);
 
-        Enrollments en = new Enrollments();
-        en.setAcademicCourse(academicCourse);
-        en.setUsers(user);
-        en.setEnrolledAt(new Date());
+        Enrollments en = Enrollments.builder()
+                .academicCourse(academicCourse)
+                .users(user)
+                .enrolledAt(new Date())
+                .build();
 
         return enrollRepository.save(en);
     }
